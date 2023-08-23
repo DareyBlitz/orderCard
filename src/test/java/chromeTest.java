b12d72ff9dca39da9169ccdc46a2c0a8308c9a9a
Darey Blitz <dareyblitz@vk.com>
2023-08-23 19:37:40 +0300

    add positive test

--- src/test/java/chromeTest.java
+++ src/test/java/chromeTest.java
@@ -38,51 +38,6 @@ public class chromeTest {
         $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
-    @Test
-    void bankCardNegativeNameTest() {
-        open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Daria Molodtsova");
-        $("[data-test-id=phone] input").setValue("+79160000000");
-        $("[data-test-id=agreement]").click();
-        $("button").click();
-        $("[data-test-id=name].input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-    }
-
-    @Test
-    void bankCardNegativePhoneTest() {
-        open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Дарья Молодцова");
-        $("[data-test-id=phone] input").setValue("89160000000");
-        $("[data-test-id=agreement]").click();
-        $("button").click();
-        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-    }
-
-    @Test
-    void bankCardNoClickTest() {
-        open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Дарья Молодцова");
-        $("[data-test-id=phone] input").setValue("+79160000000");
-        $("button").click();
-        $("[data-test-id=agreement].input_invalid .checkbox__text").shouldHave(Condition.exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
-    }
-
-    @Test
-    void bankCardEmptyNameTest() {
-        open("http://localhost:9999");
-        $("[data-test-id=phone] input").setValue("+79160000000");
-        $("[data-test-id=agreement]").click();
-        $("button").click();
-        $("[data-test-id=name].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
-    }
-
-    @Test
-    void bankCardEmptyPhoneTest() {
-        open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Дарья Молодцова");
-        $("[data-test-id=agreement]").click();
-        $("button").click();
-        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
-    }
+    
 
 }
